a3c71a3871ba73fd36650b147a75707f92d0d4d2
SrLabres <josue.labres.cruz@gmail.com>
2021-08-06 12:31:31 -0300

    Verificando usuário duplicado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+import { fauna } from '../../../services/fauna'
+import { query as q} from 'faunadb'
+
 export default NextAuth({
   providers: [
     Providers.GitHub({
@@ -9,4 +12,42 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          
+            q.If(
+              q.Not(
+                q.Exists(
+                  q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(user.email)
+                  )
+                )
+              ),
+              q.Create(
+                q.Collection('user'),
+                { data: { email }}
+              ),
+  
+              q.Get(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            )
+        )
+        return true
+      } catch (err) {
+        console.log(err)
+        return false
+      }
+
+      
+    },
+  }
 })
